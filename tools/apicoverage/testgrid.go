67b05ae480a3786afeb5819a2687ddd97a1db66d
Srinivas Hegde <srinivashegde@google.com>
2018-08-09 12:08:06 -0700

    Add api coverage tool

---
+++ tools/apicoverage/testgrid.go
@@ -0,0 +1,152 @@
+/*
+Copyright 2018 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// testgrid.go provides methods to perform action on testgrid.
+
+package main
+
+import (
+	"encoding/xml"
+	"log"
+	"os"
+)
+
+const (
+	apiCoverage    = "api_coverage"
+	overallRoute   = "OverallRoute"
+	overallConfig  = "OverallConfiguration"
+	overallService = "OverallService"
+)
+
+// TestProperty defines a property of the test
+type TestProperty struct {
+	Name  string  `xml:"name,attr"`
+	Value float32 `xml:"value,attr"`
+}
+
+// TestProperties is an array of test properties
+type TestProperties struct {
+	Property []TestProperty `xml:"property"`
+}
+
+// TestCase defines a test case that was executed
+type TestCase struct {
+	ClassName  string         `xml:"class_name,attr"`
+	Name       string         `xml:"name,attr"`
+	Time       int            `xml:"time,attr"`
+	Properties TestProperties `xml:"properties"`
+	Fail       bool           `xml:"failure,omitempty"`
+}
+
+// TestSuite defines the set of relevant test cases
+type TestSuite struct {
+	XMLName   xml.Name   `xml:"testsuite"`
+	TestCases []TestCase `xml:"testcase"`
+}
+
+func createTestProperty(value float32) TestProperty {
+	return TestProperty{Name: apiCoverage, Value: value}
+}
+
+func createTestCase(name string, properties []TestProperty, fail bool) TestCase {
+	tc := TestCase{ClassName: apiCoverage, Name: name, Properties: TestProperties{Property: properties}, Time: 0}
+	if fail {
+		tc.Fail = true
+	}
+	return tc
+}
+
+func createTestSuite(cases []TestCase) TestSuite {
+	return TestSuite{TestCases: cases}
+}
+
+func createServiceCases(coverage *OverallAPICoverage) []TestCase {
+	var tc []TestCase
+
+	var percentCovered = float32(100 * len(coverage.ServiceAPICovered) / (len(coverage.ServiceAPICovered) + len(coverage.ServiceAPINotCovered)))
+	tp := []TestProperty{createTestProperty(percentCovered)}
+	tc = append(tc, createTestCase(overallService, tp, false))
+
+	for key, value := range coverage.ServiceAPICovered {
+		tp := []TestProperty{createTestProperty(float32(value))}
+		tc = append(tc, createTestCase(overallService+"/"+key, tp, false))
+	}
+
+	for key, value := range coverage.ServiceAPINotCovered {
+		tp := []TestProperty{createTestProperty(float32(value))}
+		tc = append(tc, createTestCase(overallService+"/"+key, tp, true))
+	}
+	return tc
+}
+
+func createConfigCases(coverage *OverallAPICoverage) []TestCase {
+	var tc []TestCase
+
+	var percentCovered = float32(100 * len(coverage.ConfigurationAPICovered) / (len(coverage.ConfigurationAPICovered) + len(coverage.ConfigurationAPINotCovered)))
+	tp := []TestProperty{createTestProperty(percentCovered)}
+	tc = append(tc, createTestCase(overallConfig, tp, false))
+
+	for key, value := range coverage.ConfigurationAPICovered {
+		tp := []TestProperty{createTestProperty(float32(value))}
+		tc = append(tc, createTestCase(overallConfig+"/"+key, tp, false))
+	}
+
+	for key, value := range coverage.ConfigurationAPINotCovered {
+		tp := []TestProperty{createTestProperty(float32(value))}
+		tc = append(tc, createTestCase(overallConfig+"/"+key, tp, true))
+	}
+	return tc
+}
+
+func createRouteCases(coverage *OverallAPICoverage) []TestCase {
+	var tc []TestCase
+
+	var percentCovered = float32(100 * len(coverage.RouteAPICovered) / (len(coverage.RouteAPICovered) + len(coverage.RouteAPINotCovered)))
+	tp := []TestProperty{createTestProperty(percentCovered)}
+	tc = append(tc, createTestCase(overallRoute, tp, false))
+
+	for key, value := range coverage.RouteAPICovered {
+		tp := []TestProperty{createTestProperty(float32(value))}
+		tc = append(tc, createTestCase(overallRoute+"/"+key, tp, false))
+	}
+
+	for key, value := range coverage.RouteAPINotCovered {
+		tp := []TestProperty{createTestProperty(float32(value))}
+		tc = append(tc, createTestCase(overallRoute+"/"+key, tp, true))
+	}
+	return tc
+}
+
+func createTestgridXML(coverage *OverallAPICoverage, artifactsDir string) {
+	tc := createRouteCases(coverage)
+	tc = append(tc, createConfigCases(coverage)...)
+	tc = append(tc, createServiceCases(coverage)...)
+	ts := createTestSuite(tc)
+
+	if op, err := xml.MarshalIndent(ts, "", "  "); err != nil {
+		log.Fatalf("Error creating xml: %v", err)
+	} else {
+		outputFile := artifactsDir + "/junit_bazel.xml"
+		f, err := os.Create(outputFile)
+		if err != nil {
+			log.Fatalf("Cannot create '%s': %v", outputFile, err)
+		}
+		defer f.Close()
+		if _, err := f.WriteString(string(op) + "\n"); err != nil {
+			log.Fatalf("Cannot write to '%s': %v", f.Name(), err)
+		}
+	}
+}
