c214964fc3aaf26f753e7a3866f259706e270f87
Adriano Cunha <35786489+adrcunha@users.noreply.github.com>
2018-11-05 10:49:32 -0800

    Update scripts/dummy.go
    Co-Authored-By: srinivashegde86 <srinivashegde@google.com>

--- scripts/dummy.go
+++ scripts/dummy.go
@@ -22,5 +22,5 @@ import (
 
 func main() {
 	fmt.Println("This is a dummy go file so `go dep` can be used with knative/test-infra/scripts")
-	fmt.Println("This file can be removed once the repo contains real, useful go code in the root dir")
+	fmt.Println("This file can be safely removed if one day this directory contains real, useful go code")
 }
