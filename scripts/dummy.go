f710a703baf3ac7e5e9005693fb1c8d61c4eccbb
srinivashegde86 <srinivashegde@google.com>
2018-11-05 12:19:35 -0800

    Move dummy.go to scripts/* (#238)
    * Move dummy.go to scripts/
    
    * Update scripts/dummy.go
    
    Co-Authored-By: srinivashegde86 <srinivashegde@google.com>
    
    * Update scripts/dummy.go
    
    Co-Authored-By: srinivashegde86 <srinivashegde@google.com>
    
    * Re-add dummy.go in root dir
    
    * Add blank lines

--- scripts/dummy.go
+++ scripts/dummy.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-     https://www.apache.org/licenses/LICENSE-2.0
+    https://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,14 +14,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package main
+package scripts
 
 import (
 	"fmt"
 )
 
 func main() {
-	fmt.Println("This is a dummy go file so `go dep` can be used with knative/test-infra repo")
-	fmt.Println("This file can be removed once the repo contains real, useful go code in the root dir")
+	fmt.Println("This is a dummy go file so `go dep` can be used with knative/test-infra/scripts")
+	fmt.Println("This file can be safely removed if one day this directory contains real, useful go code")
 }
-
