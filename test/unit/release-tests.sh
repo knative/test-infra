ad44ff00d569690391cf8f781250383b5becf878
Adriano Cunha <adrcunha@google.com>
2018-12-03 11:32:55 -0800

    Allow setting GCR/GCS on command line for releases
    Using environment variables can be dangerous, flags ensure we're explicitly setting values.
    The default values point to the nightly GCR/GCS.
    
    This also implies:
    * no need for scripts to set `DOCKER_REPO_OVERRIDE` or `KO_DOCKER_REPO` by themselves
    * helper functions use the values set by the flags

--- test/unit/release-tests.sh
+++ test/unit/release-tests.sh
@@ -67,12 +67,20 @@ function mock_branch_release() {
   branch_release "$@" 2>&1
 }
 
-function call_function() {
+function call_function_pre() {
   set -e
-  local init=$1
+  local init="$1"
   shift
   eval ${init}
   "$@" 2>&1
+}
+
+function call_function_post() {
+  set -e
+  local post="$1"
+  shift
+  "$@" 2>&1
+  eval ${post}
 }
 
 echo ">> Testing initialization"
@@ -91,17 +99,37 @@ test_function 1 "error: missing release notes" initialize --release-notes
 test_function 1 "error: file a doesn't" initialize --release-notes a
 test_function 0 "" initialize --release-notes $(mktemp)
 
+test_function 1 "error: missing GCS" initialize --release-gcs
+test_function 0 "" initialize --release-gcs a --publish
+
+test_function 1 "error: missing GCR" initialize --release-gcr
+test_function 0 "" initialize --release-gcr a --publish
+
+echo ">> Testing GCR/GCS values"
+
+test_function 0 "GCR flag is ignored" call_function_post "echo :\$KO_DOCKER_REPO:" initialize --release-gcr foo
+test_function 0 "GCS flag is ignored" call_function_post "echo :\$RELEASE_GCS:" initialize --release-gcs foo
+
+test_function 0 ":ko.local:" call_function_post "echo :\$KO_DOCKER_REPO:" initialize
+test_function 0 "::" call_function_post "echo :\$RELEASE_GCS:" initialize
+
+test_function 0 ":gcr.io/knative-nightly:" call_function_post "echo :\$KO_DOCKER_REPO:" initialize --publish
+test_function 0 ":knative-nightly/test-infra:" call_function_post "echo :\$RELEASE_GCS:" initialize --publish
+
+test_function 0 ":foo:" call_function_post "echo :\$KO_DOCKER_REPO:" initialize --release-gcr foo --publish
+test_function 0 ":foo:" call_function_post "echo :\$RELEASE_GCS:" initialize --release-gcs foo --publish
+
 echo ">> Testing release branching"
 
 test_function 0 "" branch_release
-test_function 129 "usage: git tag" call_function BRANCH_RELEASE=1 branch_release
-test_function 1 "No such file" call_function BRANCH_RELEASE=1 branch_release "K Foo" "a.yaml b.yaml"
+test_function 129 "usage: git tag" call_function_pre BRANCH_RELEASE=1 branch_release
+test_function 1 "No such file" call_function_pre BRANCH_RELEASE=1 branch_release "K Foo" "a.yaml b.yaml"
 test_function 0 "release create" mock_branch_release "K Foo" "$(mktemp) $(mktemp)"
 
 echo ">> Testing validation tests"
 
 test_function 0 "Running release validation" run_validation_tests true
-test_function 0 "" call_function SKIP_TESTS=1 run_validation_tests true
+test_function 0 "" call_function_pre SKIP_TESTS=1 run_validation_tests true
 test_function 0 "i_passed" run_validation_tests "echo i_passed"
 test_function 1 "validation tests failed" run_validation_tests false
 
