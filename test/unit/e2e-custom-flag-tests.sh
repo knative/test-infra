0bd5defc525309a7e4cc885b83c443e51b14aa15
Adriano Cunha <35786489+adrcunha@users.noreply.github.com>
2019-01-25 09:52:31 -0800

    Remove outdated comment

--- test/unit/e2e-custom-flag-tests.sh
+++ test/unit/e2e-custom-flag-tests.sh
@@ -14,15 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This script runs the end-to-end tests.
-
-# If you already have a Knative cluster setup and kubectl pointing
-# to it, call this script with the --run-tests arguments and it will use
-# the cluster and run the tests.
-
-# Calling this script without arguments will create a new cluster in
-# project $PROJECT_ID, run the tests and delete the cluster.
-
 source $(dirname $0)/../../scripts/e2e-tests.sh
 
 function parse_flags() {
@@ -33,7 +24,6 @@ function parse_flags() {
   fail_test "Unexpected flag $1 passed"
 }
 
-# Script entry point.
+echo ">> Testing e2e custom flags"
 
-echo ">> Testing e2e custom flag"
 initialize --smoke-test-custom-flag
