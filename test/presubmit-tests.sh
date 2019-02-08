02d46bbd09c13f9341dc45c4777e580dbf2a2042
Gregory Haynes <greghaynes@us.ibm.com>
2019-02-08 01:53:16 +0000

    Disable md link checking
    This is failing knative/serving due to preexisting dead links.
    
    Fixes: #451

--- test/presubmit-tests.sh
+++ test/presubmit-tests.sh
@@ -21,6 +21,9 @@
 # Use the flags --build-tests, --unit-tests and --integration-tests
 # to run a specific set of tests.
 
+# See https://github.com/knative/test-infra/issues/451
+export DISABLE_MD_LINK_CHECK=1
+
 source $(dirname $0)/../scripts/presubmit-tests.sh
 
 # Run our custom build tests after the standard build tests.
