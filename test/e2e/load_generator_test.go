a35951dd9b1b05c4a88ce33be8ad76dd8c6309f9
Srinivas Hegde <srinivashegde@google.com>
2019-04-12 16:06:46 -0700

    Add loadfactors instead of numSteps

--- test/e2e/load_generator_test.go
+++ test/e2e/load_generator_test.go
@@ -25,15 +25,15 @@ import (
 	"github.com/knative/test-infra/shared/loadgenerator"
 )
 
-func loadTest(t *testing.T, numSteps int) {
+func loadTest(t *testing.T, factors []float64) {
 	opts := loadgenerator.GeneratorOptions{
 		URL:            "http://www.google.com",
 		Duration:       10 * time.Second,
-		QPS:            10,
+		BaseQPS:        10,
 		NumThreads:     1,
 		NumConnections: 1,
 		RequestTimeout: 10 * time.Second,
-		NumSteps:       numSteps,
+		LoadFactors:    factors,
 	}
 
 	res, err := opts.RunLoadTest(true)
@@ -41,21 +41,14 @@ func loadTest(t *testing.T, numSteps int) {
 		t.Fatalf("Error performing load test: %v", err)
 	}
 
-	if len(res.Result) != numSteps {
-		t.Logf("got:%d, want: %d", len(res.Result), numSteps)
-		for _, i := range res.Result {
-			if i == nil {
-				t.Fatal("nil")
-			}
-			t.Log(i.DurationHistogram.Percentiles[0].Value)
-			t.Log(i.DurationHistogram.Percentiles[0].Percentile)
-		}
+	if len(res.Result) != len(factors) {
+		t.Logf("got:%d, want: %d", len(res.Result), len(factors))
 	}
 }
 func TestLoadGeneratorFullLoad(t *testing.T) {
-	loadTest(t, 1)
+	loadTest(t, []float64{1})
 }
 
 func TestLoadGeneratorStepLoad(t *testing.T) {
-	loadTest(t, 3)
+	loadTest(t, []float64{1, 2, 4})
 }
