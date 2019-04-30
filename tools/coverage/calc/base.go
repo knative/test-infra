c76aba11485e51574a980959800a95d959c91bbe
srinivashegde86 <srinivashegde@google.com>
2019-04-30 13:13:33 -0700

    Add license header to all coverage files (#722)
    * Add license header to all coverage files
    
    * Fix test that is looking at line numbers

--- tools/coverage/calc/base.go
+++ tools/coverage/calc/base.go
@@ -1,16 +1,32 @@
 /*
-This file stores the main structs and their methods used by the coverage app
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
+
+// base.go stores the main structs and their methods used by the coverage app
+
 package calc
 
 import (
 	"fmt"
-	"github.com/knative/test-infra/tools/coverage/git"
-	"github.com/knative/test-infra/tools/coverage/githubUtil"
-	"github.com/knative/test-infra/tools/coverage/str"
 	"sort"
 	"strconv"
 	"strings"
+
+	"github.com/knative/test-infra/tools/coverage/git"
+	"github.com/knative/test-infra/tools/coverage/githubUtil"
+	"github.com/knative/test-infra/tools/coverage/str"
 )
 
 type codeBlock struct {
