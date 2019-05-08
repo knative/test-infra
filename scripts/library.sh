df7ed2a1cda67d3f31b4e66fce6d06ec73dd305b
Chi Zhang <chizhg@google.com>
2019-05-08 16:34:00 -0700

    remove istio yaml vars since they do not exsit anymore

--- scripts/library.sh
+++ scripts/library.sh
@@ -24,8 +24,6 @@ readonly SERVING_GKE_IMAGE=cos
 
 # Public latest stable nightly images and yaml files.
 readonly KNATIVE_BASE_YAML_SOURCE=https://storage.googleapis.com/knative-nightly/@/latest
-readonly KNATIVE_ISTIO_CRD_YAML=${KNATIVE_BASE_YAML_SOURCE/@/serving}/istio-crds.yaml
-readonly KNATIVE_ISTIO_YAML=${KNATIVE_BASE_YAML_SOURCE/@/serving}/istio.yaml
 readonly KNATIVE_SERVING_RELEASE=${KNATIVE_BASE_YAML_SOURCE/@/serving}/serving.yaml
 readonly KNATIVE_BUILD_RELEASE=${KNATIVE_BASE_YAML_SOURCE/@/build}/build.yaml
 readonly KNATIVE_EVENTING_RELEASE=${KNATIVE_BASE_YAML_SOURCE/@/eventing}/release.yaml
