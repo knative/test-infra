d69d88737cdf8a51a8af2400e253449ea67cee91
Adriano Cunha <35786489+adrcunha@users.noreply.github.com>
2019-05-17 09:18:06 -0700

    Also handle single quotes in make_banner()

--- scripts/library.sh
+++ scripts/library.sh
@@ -56,7 +56,7 @@ function abort() {
 #             $2 - banner message.
 function make_banner() {
     local msg="$1$1$1$1 $2 $1$1$1$1"
-    local border="${msg//[-0-9A-Za-z _.,\/()]/$1}"
+    local border="${msg//[-0-9A-Za-z _.,\/()\']/$1}"
     echo -e "${border}\n${msg}\n${border}"
 }
 
