df8391ff593f7bd01a0ea64b6dc508f3c1dfd501
Trevor Farrelly <trevorfarrelly@google.com>
2019-05-20 17:33:14 -0700

    final fix for flaky test reporter istio issues

--- tools/flaky-test-reporter/slack_notification.go
+++ tools/flaky-test-reporter/slack_notification.go
@@ -104,29 +104,28 @@ func createSlackMessageForRepo(rd *RepoData, flakyIssuesMap map[string][]*flakyI
 	flakyTests := getFlakyTests(rd)
 	message := fmt.Sprintf("As of %s, there are %d flaky tests in '%s' from repo '%s'",
 		time.Unix(*rd.LastBuildStartTime, 0).String(), len(flakyTests), rd.Config.Name, rd.Config.Repo)
+	if !rd.Config.PostIssue {
+			message += fmt.Sprintf("\n(Job is marked to not create GitHub issues)")
+	}
 	flakyRate := getFlakyRate(rd)
-	if rd.Config.PostIssue {
-		if flakyRate > threshold { // Don't list each test as this can be huge
-			message += fmt.Sprintf("\n>- skip displaying all tests as flaky rate above '%0.2f%%'", threshold)
-			if flakyIssues, ok := flakyIssuesMap[getBulkIssueIdentity(rd, flakyRate)]; ok && rd.Config.PostIssue {
-				// When flaky rate is above threshold, there is only one issue created,
-				// so there is only one element in flakyIssues
+	if flakyRate > threshold { // Don't list each test as this can be huge
+		message += fmt.Sprintf("\n>- skip displaying all tests as flaky rate above '%0.2f%%'", threshold)
+		if flakyIssues, ok := flakyIssuesMap[getBulkIssueIdentity(rd, flakyRate)]; ok && rd.Config.PostIssue {
+			// When flaky rate is above threshold, there is only one issue created,
+			// so there is only one element in flakyIssues
+			for _, fi := range flakyIssues {
+				message += fmt.Sprintf("\t%s", fi.issue.GetHTMLURL())
+			}
+		}
+	} else {
+		for _, testFullName := range flakyTests {
+			message += fmt.Sprintf("\n>- %s", testFullName)
+			if flakyIssues, ok := flakyIssuesMap[getIdentityForTest(testFullName, rd.Config.Repo)]; ok && rd.Config.PostIssue {
 				for _, fi := range flakyIssues {
 					message += fmt.Sprintf("\t%s", fi.issue.GetHTMLURL())
 				}
 			}
-		} else {
-			for _, testFullName := range flakyTests {
-				message += fmt.Sprintf("\n>- %s", testFullName)
-				if flakyIssues, ok := flakyIssuesMap[getIdentityForTest(testFullName, rd.Config.Repo)]; ok && rd.Config.PostIssue {
-					for _, fi := range flakyIssues {
-						message += fmt.Sprintf("\t%s", fi.issue.GetHTMLURL())
-					}
-				}
-			}
 		}
-	} else {
-		message += fmt.Sprintf("\t Job is marked to not create GitHub issues")
 	}
 
 	if testgridTabURL, err := testgrid.GetTestgridTabURL(rd.Config.Name, []string{testgridFilter}); nil != err {
