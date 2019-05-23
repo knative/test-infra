392963bbd5512b445269faa9c487d9a49f0f9dfd
Adriano Cunha <35786489+adrcunha@users.noreply.github.com>
2019-05-22 17:01:27 -0700

    Move /tools/devstats to /devstats (#819)
    When vendoring test-infra in other repos, `dep` leaves a few json files from `tools/devstats`.
    
    Furthermore, technically `devstats` is not a tool but a completely separate engine.

--- devstats/scripts/setup_mount.sh
+++ devstats/scripts/setup_mount.sh
@@ -22,7 +22,7 @@ git clone https://github.com/knative/serving.git /mount/data/devstats_repos/knat
 
 cd /mount/data/src/
 git clone https://github.com/knative/test-infra.git
-cd test-infra/tools/devstats
+cd test-infra/devstats
 ./scripts/copy_devstats_binaries.sh
 
-rm -rf /etc/gha2db && ln -sf /mount/data/src/test-infra/tools/devstats /etc/gha2db
+rm -rf /etc/gha2db && ln -sf /mount/data/src/test-infra/devstats /etc/gha2db
