d849bb2be9ee0cdfcd9016af0c76455f40d9e481
Adriano Cunha <adrcunha@google.com>
2019-06-13 08:51:03 -0700

    Do fail coverage job on errors
    Bonus: fix copyright header in 2 files.

--- tools/coverage/logUtil/logUtil.go
+++ tools/coverage/logUtil/logUtil.go
@@ -17,12 +17,10 @@ package logUtil
 
 import (
 	"log"
-	"os"
 )
 
 // LogFatalf customizes the behavior of handling Fatal error
+// This is also used for testing code failure behavior.
 var LogFatalf = func(format string, v ...interface{}) {
-	log.Printf(format, v...)
-	log.Println("Exit program with code 0 regardless of the above error")
-	os.Exit(0)
+	log.Fatalf(format, v...)
 }
