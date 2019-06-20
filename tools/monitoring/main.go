d5525178a44ee8557ff45b4fa6b688edf2d39fe3
Stephen <36011612+steuhs@users.noreply.github.com>
2019-06-19 18:19:15 -0700

    fix build breakage & print error to http response instead of console

--- tools/monitoring/main.go
+++ tools/monitoring/main.go
@@ -113,15 +113,17 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 func testCloudSQLConn(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
-	fmt.Fprintf(w,"Testing mysql database connection...")
+	fmt.Fprintf(w, "Testing mysql database connection...")
 
 	db, err := dbConfig.Connect()
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Fprintf(w, "Failed to ping the database %v", err)
+		return
 	}
 
 	fmt.Fprintf(w, "testing alert condition check...")
-	toAlert, err := config.SelectedConfig{}.CheckAlertCondition("none", db)
+	s := config.SelectedConfig{}
+	toAlert, err := s.CheckAlertCondition("none", db)
 	if err != nil {
 		fmt.Fprintf(w, "error running alert condition check in no-match senario: %v", err)
 	} else if toAlert {
