5ecff04ed53b15f443ca3304bedef411070c41f7
Cory Cross <coryrc@google.com>
2019-06-28 17:39:46 -0700

    typo

--- ci/prow/boskos/permissions.sh
+++ ci/prow/boskos/permissions.sh
@@ -3,7 +3,7 @@
 # Copyright 2019 The Knative Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-r# you may not use this file except in compliance with the License.
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
