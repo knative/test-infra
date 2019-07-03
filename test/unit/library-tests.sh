75ed4e6682deef6d940384ebc416896414ffcbfe
Adriano Cunha <adrcunha@google.com>
2019-07-02 17:10:45 -0700

    Fix formatting of repo names with hyphen
    Bonus: remove leftover bazel cleanup from library-tests.sh.

--- test/unit/library-tests.sh
+++ test/unit/library-tests.sh
@@ -37,18 +37,13 @@ function test_report() {
   grepit "XML report written"
 }
 
-# Cleanup bazel stuff to avoid confusing Prow
-function cleanup_bazel() {
-  bazel clean
-}
-
-trap cleanup_bazel EXIT
-
 echo ">> Testing helper functions"
+
+test_function ${SUCCESS} "Knative Test Infra" echo "${REPO_NAME_FORMATTED}"
 
 test_function ${SUCCESS} "${REPO_ROOT_DIR}/test/unit/library-tests.sh" get_canonical_path test/unit/library-tests.sh
 test_function ${SUCCESS} "Foo Bar" capitalize "foo bar"
-test_function ${SUCCESS} ">>> Knative Testinfra controller logs:" mock_kubectl_function dump_app_logs "controller" "test-infra"
+test_function ${SUCCESS} ">>> Knative Test Infra controller logs:" mock_kubectl_function dump_app_logs "controller" "test-infra"
 
 test_function ${SUCCESS} "" is_protected_gcr "gcr.io/knative-releases"
 test_function ${SUCCESS} "" is_protected_gcr "gcr.io/knative-nightly"
