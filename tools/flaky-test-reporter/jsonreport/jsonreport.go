8364b016f55d4ffb73158e75d79dfc4e4694e83e
Trevor Farrelly <trevorfarrelly@google.com>
2019-07-08 14:34:44 -0700

    refactor jsonreport to better support multi-repo searching (#1105)
    * jsonreport: add function for getting repos that are reported on
    
    * jsonreport: refactor to better support possible uses
    
    * short-circuit checking latest build
    
    * jsonreport: update buildID instead of recursing
    
    * jsonreport: fix comments

--- tools/flaky-test-reporter/jsonreport/jsonreport.go
+++ tools/flaky-test-reporter/jsonreport/jsonreport.go
@@ -22,6 +22,7 @@ import (
 	"io/ioutil"
 	"path"
 	"sort"
+	"strings"
 	"time"
 
 	"github.com/knative/test-infra/shared/common"
@@ -34,9 +35,9 @@ const (
 	maxAge   = 4                            // maximum age in days that JSON data is valid
 )
 
-// Report contains concise information about current flaky tests
+// Report contains concise information about current flaky tests in a given repo
 type Report struct {
-	Repo  string   `json:"-"`
+	Repo  string   `json:"repo"`
 	Flaky []string `json:"flaky"`
 }
 
@@ -45,6 +46,7 @@ func Initialize(serviceAccount string) error {
 	return prow.Initialize(serviceAccount)
 }
 
+// writeToArtifactsDir writes the flaky test data for this repo to disk.
 func (r *Report) writeToArtifactsDir() error {
 	artifactsDir := prow.GetLocalArtifactsDir()
 	if err := common.CreateDir(path.Join(artifactsDir, r.Repo)); nil != err {
@@ -58,49 +60,81 @@ func (r *Report) writeToArtifactsDir() error {
 	return ioutil.WriteFile(outFilePath, contents, 0644)
 }
 
-func getJSONForBuild(repo string, buildID int) ([]byte, error) {
-	relPath := path.Join(prow.ArtifactsDir, repo, filename)
+// GetFlakyTestReport collects flaky test reports from the given buildID and repo.
+// Use repo = "" to get reports from all repositories, and buildID = -1 to get the
+// most recent report
+func GetFlakyTestReport(repo string, buildID int) ([]Report, error) {
 	job := prow.NewJob(jobName, prow.PeriodicJob, "", 0)
-	if buildID != -1 {
-		return job.NewBuild(buildID).ReadFile(relPath)
+	var err error
+	if buildID == -1 {
+		buildID, err = getLatestValidBuild(job, repo)
+		if err != nil {
+			return nil, err
+		}
 	}
-	return getLatestValidJSON(job, relPath)
+	build := job.NewBuild(buildID)
+	var reports []Report
+	for _, filepath := range getReportPaths(build, repo) {
+		report, err := readJSONReport(build, filepath)
+		if err != nil {
+			return nil, err
+		}
+		reports = append(reports, *report)
+	}
+	return reports, nil
 }
 
-// inexpensively sort and find the most recent JSON file. Assumes build IDs are in sequential order.
-func getLatestValidJSON(job *prow.Job, relPath string) ([]byte, error) {
+// getLatestValidBuild inexpensively sorts and finds the most recent JSON report.
+// Assumes sequential build IDs are sequential in time.
+func getLatestValidBuild(job *prow.Job, repo string) (int, error) {
+	// check latest build first, before looking to older builds
+	if buildID, err := job.GetLatestBuildNumber(); err == nil {
+		build := job.NewBuild(buildID)
+		if reports := getReportPaths(build, repo); len(reports) != 0 {
+			return buildID, nil
+		}
+	}
+	// look at older builds
 	maxElapsedTime, _ := time.ParseDuration(fmt.Sprintf("%dh", maxAge*24))
 	buildIDs := job.GetBuildIDs()
 	sort.Sort(sort.Reverse(sort.IntSlice(buildIDs)))
 	for _, buildID := range buildIDs {
 		build := job.NewBuild(buildID)
-		// check for JSON
-		if contents, err := build.ReadFile(relPath); err != nil {
+		// check if reports exist for this build
+		if reports := getReportPaths(build, repo); len(reports) == 0 {
 			continue
-		} else {
-			// JSON exists, check timestamp
-			startTimeInt, err := build.GetStartTime()
-			if err != nil {
-				continue
-			}
-			startTime := time.Unix(startTimeInt, 0)
-			if time.Since(startTime) < maxElapsedTime {
-				return contents, nil
-			} else {
-				return nil, fmt.Errorf("latest JSON log is outdated: %.2f days old", time.Since(startTime).Hours()/24)
-			}
 		}
+		// check if this report is too old
+		startTimeInt, err := build.GetStartTime()
+		if err != nil {
+			continue
+		}
+		startTime := time.Unix(startTimeInt, 0)
+		if time.Since(startTime) < maxElapsedTime {
+			return buildID, nil
+		}
+		return 0, fmt.Errorf("latest JSON log is outdated: %.2f days old", time.Since(startTime).Hours()/24)
 	}
-	return nil, fmt.Errorf("no JSON logs found in recent builds")
+	return 0, fmt.Errorf("no JSON logs found in recent builds")
 }
 
-// GetReportForRepo gets a Report struct from a specific build for a given repo
-// use buildID = -1 for most recent successful report
-func GetReportForRepo(repo string, buildID int) (*Report, error) {
-	report := &Report{
-		Repo: repo,
+// getReportPaths searches build artifacts for reports from the given repo, returning
+// the path to any matching files. Use repo = "" to get all reports from all repos.
+func getReportPaths(build *prow.Build, repo string) []string {
+	var matches []string
+	suffix := path.Join(repo, filename)
+	for _, artifact := range build.GetArtifacts() {
+		if strings.HasSuffix(artifact, suffix) {
+			matches = append(matches, artifact)
+		}
 	}
-	contents, err := getJSONForBuild(repo, buildID)
+	return matches
+}
+
+// readJSONReport builds a repo-specific report object from a given json file path.
+func readJSONReport(build *prow.Build, filename string) (*Report, error) {
+	report := &Report{}
+	contents, err := build.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
@@ -110,7 +144,8 @@ func GetReportForRepo(repo string, buildID int) (*Report, error) {
 	return report, nil
 }
 
-// CreateReportForRepo generates a Report struct and optionally writes it to disk
+// CreateReportForRepo generates a flaky report for a given repository, and optionally
+// writes it to disk.
 func CreateReportForRepo(repo string, flaky []string, writeFile bool) (*Report, error) {
 	report := &Report{
 		Repo:  repo,
