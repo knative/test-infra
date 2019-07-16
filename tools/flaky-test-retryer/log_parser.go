96798a5f3dee37bf94d86b2d8b8ceb13abbcb35f
Trevor Farrelly <trevorfarrelly@google.com>
2019-07-16 13:27:56 -0700

    cache log results to limit API calls

--- tools/flaky-test-retryer/log_parser.go
+++ tools/flaky-test-retryer/log_parser.go
@@ -27,6 +27,9 @@ import (
 	"github.com/knative/test-infra/shared/junit"
 	"github.com/knative/test-infra/shared/prow"
 	"github.com/knative/test-infra/tools/flaky-test-reporter/jsonreport"
+	// TODO: remove this import once "k8s.io/test-infra" import problems are fixed
+	// https://github.com/test-infra/test-infra/issues/912
+	"github.com/knative/test-infra/tools/monitoring/prowapi"
 )
 
 // InitLogParser configures jsonreport's dependencies.
@@ -34,9 +37,66 @@ func InitLogParser(serviceAccount string) error {
 	return jsonreport.Initialize(serviceAccount)
 }
 
+// JobData contains stripped-down information about a failed job, and local caches
+// of its failed tests and the flaky report it is referencing.
+type JobData struct {
+	message      *prowapi.ReportMessage
+	failedTests  []string
+	flakyReports []jsonreport.Report
+}
+
+// NewJobData creates a JobData object for the given message, and returns an error
+// if it does not fit the criteria we have set for it.
+func NewJobData(msg *prowapi.ReportMessage) *JobData {
+	return &JobData{message: msg}
+}
+
+// IsSupported checks to make sure the message can be processed with the current flaky
+// test information
+func (jd *JobData) IsSupported() error {
+	if jd.message.Status != prowapi.FailureState {
+		return fmt.Errorf("Was not a failure: %v\n", jd.message.Status)
+	}
+	// check type
+	if jd.message.JobType != prowapi.PresubmitJob {
+		return fmt.Errorf("Was not presubmit: %v\n", jd.message.JobType)
+	}
+	// check repo
+	if len(jd.message.Refs) == 0 {
+		return fmt.Errorf("No ref in message.\n")
+	}
+	repos, err := jd.getReportRepos()
+	if err != nil {
+		return err
+	}
+	expRepo := false
+	for _, repo := range repos {
+		if jd.message.Refs[0].Repo == repo {
+			expRepo = true
+			break
+		}
+	}
+	if !expRepo {
+		return fmt.Errorf("Repo unsupported: %v\n", jd.message.Refs[0].Repo)
+	}
+	// make sure pull ID exists
+	if len(jd.message.Refs[0].Pulls) == 0 {
+		return fmt.Errorf("No pull ID in message\n")
+	}
+	return nil
+}
+
+func (jd *JobData) String() string {
+	return fmt.Sprintf("%s/pull/%d: %s", jd.message.Refs[0].Repo, jd.message.Refs[0].Pulls[0].Number, jd.message.JobName)
+}
+
 // getFailedTests gets all the tests that failed in the given job.
-func getFailedTests(jobName, jobType, repo string, pull int) ([]string, error) {
-	job := prow.NewJob(jobName, jobType, repo, pull)
+func (jd *JobData) getFailedTests() ([]string, error) {
+	// use cache if it is populated
+	if jd.failedTests != nil {
+		return jd.failedTests, nil
+	}
+	job := prow.NewJob(jd.message.JobName, string(jd.message.JobType), jd.message.Refs[0].Repo, jd.message.Refs[0].Pulls[0].Number)
 	buildID, err := job.GetLatestBuildNumber()
 	if err != nil {
 		return nil, err
@@ -56,6 +116,7 @@ func getFailedTests(jobName, jobType, repo string, pull int) ([]string, error) {
 			}
 		}
 	}
+	jd.failedTests = tests
 	return tests, nil
 }
 
@@ -85,29 +146,33 @@ func GetCombinedResultsForBuild(build *prow.Build) ([]*junit.TestSuites, error) 
 	return allSuites, nil
 }
 
-// getFlakyTests gets the latest flaky tests for the given repository.
-func getFlakyTests(repo string) ([]string, error) {
-	return parseFlakyLog(func(report jsonreport.Report, result *[]string) {
-		if report.Repo == repo {
+// getFlakyTests gets the latest flaky tests that could affect this job
+func (jd *JobData) getFlakyTests() ([]string, error) {
+	return jd.parseFlakyLog(func(report jsonreport.Report, result *[]string) {
+		if report.Repo == jd.message.Refs[0].Repo {
 			*result = report.Flaky
 		}
 	})
 }
 
 // getReportRepos gets all of the repositories where we collect flaky tests.
-func getReportRepos() ([]string, error) {
-	return parseFlakyLog(func(report jsonreport.Report, result *[]string) {
+func (jd *JobData) getReportRepos() ([]string, error) {
+	return jd.parseFlakyLog(func(report jsonreport.Report, result *[]string) {
 		*result = append(*result, report.Repo)
 	})
 }
 
 // parseFlakyLog reads the latest flaky test report and returns filtered results based
 // on the function the caller passes in.
-func parseFlakyLog(f func(report jsonreport.Report, result *[]string)) ([]string, error) {
+func (jd *JobData) parseFlakyLog(f func(report jsonreport.Report, result *[]string)) ([]string, error) {
 	var results []string
-	reports, err := jsonreport.GetFlakyTestReport("", -1)
-	if err == nil && len(reports) > 0 {
-		for _, r := range reports {
+	var err error
+	// populate cache if it is empty
+	if jd.flakyReports == nil {
+		jd.flakyReports, err = jsonreport.GetFlakyTestReport("", -1)
+	}
+	if err == nil && len(jd.flakyReports) > 0 {
+		for _, r := range jd.flakyReports {
 			f(r, &results)
 		}
 	}
