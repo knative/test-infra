55cb7e5100b631e3cfcc7c262bb14770e5dac7cd
Srinivas Hegde <srinivashegde@google.com>
2019-07-21 14:26:10 -0700

    Remove GCS URL hack

--- tools/monitoring/alert/alert.go
+++ tools/monitoring/alert/alert.go
@@ -68,7 +68,6 @@ func (c *Client) handleReportMessage(rmsg *prowapi.ReportMessage) {
 			return
 		}
 
-		rmsg.GCSPath = toGcsLink(rmsg.GCSPath)
 		blPath, err := gcs.BuildLogPath(rmsg.GCSPath)
 		if err != nil {
 			log.Printf("Failed to construct build log url from gcs path %s. Error: %v\n", rmsg.GCSPath, err)
@@ -120,12 +119,6 @@ func (c *Client) handleSingleError(config *config.Config, rmsg *prowapi.ReportMe
 	if err != nil {
 		log.Printf("Failed to Alert %v", err)
 	}
-}
-
-// TODO(yt3liu): Remove this hack after the gcs path does not contain extra link prefix
-func toGcsLink(link string) string {
-	return strings.Replace(strings.Replace(link, "https://gubernator.knative.dev/build/", "", 1),
-		"https://prow.knative.dev/view/gcs/", "", 1)
 }
 
 func (m *MailConfig) sendAlert(c *mailContent) error {
