aedb45d0481bddedb259f6d4f4f6499b14c879f9
Stephen <36011612+steuhs@users.noreply.github.com>
2019-08-30 12:44:14 -0700

    address Joyce's comment

--- tools/coverage/githubUtil/github_test.go
+++ tools/coverage/githubUtil/github_test.go
@@ -20,31 +20,31 @@ import (
 	"testing"
 )
 
-func TestFilePathProfileToGithub(t *testing.T) {
-	t.Run("repo=knative.dev", func(t *testing.T) {
-		input := "knative.dev/test-infra/pkg/ab/cde"
-		expectedOutput := "pkg/ab/cde"
-		actualOutput := FilePathProfileToGithub(input)
-		if actualOutput != expectedOutput {
-			t.Fatalf("input=%s; expected output=%s; actual output=%s", input, expectedOutput,
-				actualOutput)
-		}
-	})
-	t.Run("repo=github.com/{repo}", func(t *testing.T) {
-		input := "github.com/myRepoOwner/myRepoName/pkg/ab/cde"
-		expectedOutput := "pkg/ab/cde"
-		repoRoot := "/d1/d2/d3/gopath/src/github.com/myRepoOwner/myRepoName"
-		getRepoRoot = func() (string, error) {
-			return repoRoot, nil
-		}
-		gopath := os.Getenv("GOPATH")
-		os.Setenv("GOPATH", "/d1/d2/d3/gopath")
-		defer os.Setenv("GOPATH", gopath)
-		actualOutput := FilePathProfileToGithub(input)
+func TestFilePathProfileToGithubUnmocked(t *testing.T) {
+	// this un-mocked version also verifies getRepoRoot
+	input := "knative.dev/test-infra/pkg/ab/cde"
+	expectedOutput := "pkg/ab/cde"
+	actualOutput := FilePathProfileToGithub(input)
+	if actualOutput != expectedOutput {
+		t.Errorf("FilePathProfileToGithub(%s) = %s; want %s", input, actualOutput,
+			expectedOutput)
+	}
+}
 
-		if actualOutput != expectedOutput {
-			t.Fatalf("input=%s; expected output=%s; actual output=%s", input, expectedOutput,
-				actualOutput)
-		}
-	})
+func TestFilePathProfileToGithub(t *testing.T) {
+	input := "github.com/myRepoOwner/myRepoName/pkg/ab/cde"
+	expectedOutput := "pkg/ab/cde"
+	repoRoot := "/d1/d2/d3/gopath/src/github.com/myRepoOwner/myRepoName"
+	getRepoRoot = func() (string, error) {
+		return repoRoot, nil
+	}
+	gopath := os.Getenv("GOPATH")
+	os.Setenv("GOPATH", "/d1/d2/d3/gopath")
+	defer os.Setenv("GOPATH", gopath)
+	actualOutput := FilePathProfileToGithub(input)
+
+	if actualOutput != expectedOutput {
+		t.Errorf("FilePathProfileToGithub(%s) = %s; want %s", input,
+			actualOutput, expectedOutput)
+	}
 }
