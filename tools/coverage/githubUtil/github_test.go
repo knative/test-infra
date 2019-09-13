ca04b8453a8779f3c800c5e4bcda43ff53ec6f80
Stephen L <36011612+steuhs@users.noreply.github.com>
2019-09-12 17:02:34 -0700

    [coverage] update file path converter (#1290)
    * [coverage] update string mapper to accommodate repo structure change
    
    The change of repo structure from github.com/$REPO_OWNER/$REPO_NAME to knative.dev/$REPO_NAME/ breaks the function FilePathProfileToGithub, because the function is based on the repo structure. This change is to fix the issue.
    
    * [coverage] update file path converter
    
    The change of repo structure from github.com/$REPO_OWNER/$REPO_NAME to knative.dev/$REPO_NAME/ breaks the function FilePathProfileToGithub, because the function is based on the repo structure.
    This change introduced a more adaptive way to make the path conversion. It no longer relies on how the where and how the repo is hosted. Instead it uses repository root and GOPATH to determine exact prefix to remove to make the conversion.
    
    * nit
    
    * nit 2
    
    * added test case for additional repo structure
    
    * address Joyce's comment
    
    * changed test cases to tabular form and mocked both scenarios
    
    * reset getRepoRoot to unmocked version after test
    
    * fix
    
    * add doc string
    
    * improve defer func

--- tools/coverage/githubUtil/github_test.go
+++ tools/coverage/githubUtil/github_test.go
@@ -16,15 +16,47 @@ limitations under the License.
 package githubUtil
 
 import (
+	"os"
 	"testing"
 )
 
 func TestFilePathProfileToGithub(t *testing.T) {
-	input := "github.com/myRepoOwner/myRepoName/pkg/ab/cde"
-	expectedOutput := "pkg/ab/cde"
-	actualOutput := FilePathProfileToGithub(input)
-	if actualOutput != expectedOutput {
-		t.Fatalf("input=%s; expected output=%s; actual output=%s", input, expectedOutput,
-			actualOutput)
+	type args struct {
+		file     string
+		gopath   string
+		repoRoot string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"repo on github.com",
+			args{"github.com/myRepoOwner/myRepoName/pkg/ab/cde",
+				"/d1/d2/d3/gopath",
+				"/d1/d2/d3/gopath/src/github.com/myRepoOwner/myRepoName"},
+			"pkg/ab/cde"},
+		{"repo on knative.dev",
+			args{"knative.dev/test-infra/pkg/ab/cde",
+				"/d1/d2/gopath",
+				"/d1/d2/gopath/src/knative.dev/test-infra"},
+			"pkg/ab/cde"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gopath := os.Getenv("GOPATH")
+			os.Setenv("GOPATH", tt.args.gopath)
+			getRepoRootSaved := getRepoRoot
+			getRepoRoot = func() (string, error) {
+				return tt.args.repoRoot, nil
+			}
+			defer func(){
+			    os.Setenv("GOPATH", gopath)
+			    getRepoRoot = getRepoRootSaved
+			}()
+			if got := FilePathProfileToGithub(tt.args.file); got != tt.want {
+				t.Errorf("FilePathProfileToGithub(%v) = %v, want %v", tt.args.file, got, tt.want)
+			}
+		})
 	}
 }
