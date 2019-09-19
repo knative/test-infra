644d6ff9d3c66817660860380b62fe00f83746f6
Adriano Cunha <35786489+adrcunha@users.noreply.github.com>
2019-09-19 12:08:41 -0700

    library.sh already sets GOPATH if necessary

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -18,10 +18,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [ -z "${GOPATH:-}" ]; then
-  export GOPATH=$(go env GOPATH)
-fi
-
 source $(dirname $0)/../scripts/library.sh
 
 # Make sure our dependencies are up-to-date
