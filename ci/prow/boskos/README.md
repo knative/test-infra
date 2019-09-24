49a31ce2454e58e47a9c34f62ce8ea4d43f58535
Chi Zhang <chizhg@google.com>
2019-09-23 17:47:13 -0700

    no custom role is needed for now

--- ci/prow/boskos/README.md
+++ ci/prow/boskos/README.md
@@ -5,8 +5,6 @@ This directory contains the config for our
 
 - `config_start.yaml` Initial Boskos configuration.
 - `config.yaml` Boskos configuration.
-- `create_projects.sh` Script to create new Boskos projects and set permissions on them.
+- `create_projects.sh` Script to create new Boskos projects.
+- `set_permissions.sh` Script to set up permissions for a Boskos project.
 - `resources.yaml` Pool of projects used by Boskos.
-- `custom_role.yaml` Configuration of the custom role, needs to be updated whenever we change its permissions.
-- `make_custom_role.sh` Script to generate the custom role configuration file.
-- `update_custom_role.sh` Script to update the custom role for all existing boskos projects.
--- ci/prow/boskos/create_projects.sh
+++ ci/prow/boskos/create_projects.sh
@@ -19,22 +19,9 @@ set -e
 readonly NUMBER=${1:?"First argument is the number of new projects to create."}
 readonly BILLING_ACCOUNT=${2:?"Second argument must be the billing account."}
 
-readonly CUSTOM_ROLE_NAME="KnativeIntegrationTestsRunner"
-readonly CUSTOM_ROLE_FILE="custom_role.yaml"
+readonly RESOURCE_FILE="resources.yaml"
 
-readonly PROJECT_OWNERS=("prime-engprod-sea@google.com")
-readonly PROJECT_GROUPS=("knative-productivity-admins@googlegroups.com")
-readonly PROJECT_SAS=(
-    "knative-tests@appspot.gserviceaccount.com"
-    "prow-job@knative-tests.iam.gserviceaccount.com"
-    "prow-job@knative-nightly.iam.gserviceaccount.com"
-    "prow-job@knative-releases.iam.gserviceaccount.com")
-readonly PROJECT_APIS=(
-    "cloudresourcemanager.googleapis.com"
-    "compute.googleapis.com"
-    "container.googleapis.com")
-
-start=$(grep -n "knative-boskos-" resources.yaml | wc -l)
+start=$(grep -n "knative-boskos-" ${RESOURCE_FILE} | wc -l)
 ((start++))
 for (( i=0; i<${NUMBER}; i++ )); do
   PROJECT="knative-boskos-$(( ${start} + i ))"
@@ -44,41 +31,10 @@ for (( i=0; i<${NUMBER}; i++ )); do
   gcloud projects create ${PROJECT} --folder=396521612403
   gcloud beta billing projects link ${PROJECT} --billing-account=${BILLING_ACCOUNT}
 
-  # Set permissions for users on this new project
-  # Add an owner to the PROJECT
-  for owner in ${PROJECT_OWNERS[@]}; do
-    echo "NOTE: Adding owner ${owner}"
-    gcloud projects add-iam-policy-binding ${PROJECT} --member group:${owner} --role roles/owner
-  done
+  # Set permissions for this project
+  "$(dirname $0)/set_permissions.sh" ${PROJECT}
 
-  # Add all GROUPS as editors
-  for group in ${PROJECT_GROUPS[@]}; do
-    echo "NOTE: Adding group ${group}"
-    gcloud projects add-iam-policy-binding ${PROJECT} --member group:${group} --role roles/editor
-  done
-
-  # Create the custom role in this new project
-  gcloud iam roles create ${CUSTOM_ROLE_NAME} -q --project ${PROJECT} --file ${CUSTOM_ROLE_FILE}
-  for sa in ${PROJECT_SAS[@]}; do
-    echo "NOTE: Adding service account ${sa}"
-    # Bind the custom role to the SA
-    gcloud projects add-iam-policy-binding ${PROJECT} --member serviceAccount:${sa} --role projects/${PROJECT}/roles/${CUSTOM_ROLE_NAME}
-    # As required by step 6 in https://github.com/google/knative-gcp/tree/master/docs/storage,
-    # grant the GCS service account the permissions to publish to GCP Pub/Sub
-    gcs_service_account="$(curl -s -X GET -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" "https://www.googleapis.com/storage/v1/projects/${PROJECT}/serviceAccount" \
-      | grep email_address \
-      | cut -d '"' -f 4)"
-    gcloud projects add-iam-policy-binding ${PROJECT} \
-      --member=serviceAccount:${gcs_service_account} \
-      --role roles/pubsub.publisher
-  done
-
-  # Enable APIS
-  for api in ${PROJECT_APIS[@]}; do
-    echo "NOTE: Enabling API ${api}"
-    gcloud services enable ${api} --project=${PROJECT}
-  done
-  last_project_line=$(grep -n "knative-boskos-" resources.yaml | tail -n 1 | cut -d: -f1)
+  last_project_line=$(grep -n "knative-boskos-" ${RESOURCE_FILE} | tail -n 1 | cut -d: -f1)
   ((last_project_line++))
-  sed -e "${last_project_line}i\ \ - ${PROJECT}" -i resources.yaml
+  sed -e "${last_project_line}i\ \ - ${PROJECT}" -i ${RESOURCE_FILE}
 done
