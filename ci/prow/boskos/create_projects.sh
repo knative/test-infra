5b0e0862d61f878164fc27c701421b525132544e
Chi Zhang <chizhg@google.com>
2019-09-25 18:20:06 -0700

    a few improvements for boskos scripts

--- ci/prow/boskos/create_projects.sh
+++ ci/prow/boskos/create_projects.sh
@@ -16,18 +16,29 @@
 
 set -e
 
-readonly FIRST=${1:?"First argument is the first number of the new project(s)."}
-readonly NUMBER=${2:?"Second argument is the number of new projects."}
-readonly BILLING_ACCOUNT=${3:?"Third argument must be the billing account."}
-readonly OUTPUT_FILE=${4:?"Fourth argument should be a file name all project names will be appended to in a resources.yaml format."}
+readonly NUMBER=${1:?"First argument is the number of new projects to create."}
+readonly BILLING_ACCOUNT=${2:?"Second argument must be the billing account."}
 
+readonly RESOURCE_FILE="resources.yaml"
+
+if [[ ! -f ${RESOURCE_FILE} || ! -w ${RESOURCE_FILE} ]]; then
+  echo "${RESOURCE_FILE} does not exist or is not writable"
+fi
+
+# Get the index of the last boskos project from the resources file
+START=$(grep -n "knative-boskos-" ${RESOURCE_FILE} | wc -l)
+((START++))
 for (( i=0; i<${NUMBER}; i++ )); do
-  PROJECT="knative-boskos-$(( i + ${FIRST} ))"
+  PROJECT="knative-boskos-$(( ${START} + i ))"
   # This Folder ID is google.com/google-default
   # If this needs to be changed for any reason, GCP project settings must be updated.
   # Details are available in Google's internal issue 137963841.
   gcloud projects create ${PROJECT} --folder=396521612403
   gcloud beta billing projects link ${PROJECT} --billing-account=${BILLING_ACCOUNT}
+
+  # Set permissions for this project
   "$(dirname $0)/set_permissions.sh" ${PROJECT}
-  echo "  - ${PROJECT}" >> ${OUTPUT_FILE}
+
+  last_project=$(grep "knative-boskos-" resources.yaml | tail -1)
+  sed "/${last_project}/a\ \ -\ ${PROJECT}" -i ${RESOURCE_FILE}
 done
