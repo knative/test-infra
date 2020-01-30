b91919099d456bac5d78861c3ee260661c9a487d
Matt Moore <mattomata@gmail.com>
2020-01-30 09:57:18 -0800

    Apply suggestions from code review
    Co-Authored-By: Adriano Cunha <35786489+adrcunha@users.noreply.github.com>

--- tools/cleanup/cleanup.go
+++ tools/cleanup/cleanup.go
@@ -44,7 +44,7 @@ import (
 
 var (
 	// Authentication method for using Google Cloud Registry APIs.
-	author = authn.DefaultKeychain
+	auther = authn.DefaultKeychain
 
 	// Alias of remote.Delete for testing purposes.
 	remoteDelete = remote.Delete
@@ -162,7 +162,7 @@ func (d* ImageDeleter) deleteImage(ref string) error {
 	if err != nil {
 		return errors.Wrapf(err, "failed to parse reference %q", ref)
 	}
-	if err := remoteDelete(image, remote.WithAuthFromKeychain(author)); err != nil {
+	if err := remoteDelete(image, remote.WithAuthFromKeychain(auther)); err != nil {
 		return errors.Wrapf(err, "failed to delete %q", image)
 	}
 	return nil
@@ -250,7 +250,7 @@ func (d *ImageDeleter) DeleteResources(project string, hoursToKeepResource int, 
 			}
 		}
 		return nil
-	}, google.WithAuthFromKeychain(author))
+	}, google.WithAuthFromKeychain(auther))
 }
 
 // Projects returns the projects that should be cleaned up by a ResourceDeleter.
