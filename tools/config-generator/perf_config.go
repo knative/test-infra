1a17cfe23d610f5ed3ac2513059bbc7c961df0e6
coryrc <coryrc@google.com>
2020-05-11 20:11:01 -0700

    Update Prow jobs to include GO_VERSION env var (#2051)
    Informs gvm how to choose which version of Go to use
    
    Is a no-op until we switch the image.

--- tools/config-generator/perf_config.go
+++ tools/config-generator/perf_config.go
@@ -105,9 +105,9 @@ func perfClusterBaseProwJob(command string, args []string, fullRepoName, sa stri
 	base.Command = command
 	base.Args = args
 	addVolumeToJob(&base, "/etc/performance-test", sa, true, "")
-	addEnvToJob(&base, "GOOGLE_APPLICATION_CREDENTIALS", "/etc/performance-test/service-account.json")
-	addEnvToJob(&base, "GITHUB_TOKEN", "/etc/performance-test/github-token")
-	addEnvToJob(&base, "SLACK_READ_TOKEN", "/etc/performance-test/slack-read-token")
-	addEnvToJob(&base, "SLACK_WRITE_TOKEN", "/etc/performance-test/slack-write-token")
+	base.addEnvToJob("GOOGLE_APPLICATION_CREDENTIALS", "/etc/performance-test/service-account.json")
+	base.addEnvToJob("GITHUB_TOKEN", "/etc/performance-test/github-token")
+	base.addEnvToJob("SLACK_READ_TOKEN", "/etc/performance-test/slack-read-token")
+	base.addEnvToJob("SLACK_WRITE_TOKEN", "/etc/performance-test/slack-write-token")
 	return base
 }
