8bc6cf11761f72c4551d6d31d58239b10ec0b3da
yizong <yijianzong@gmail.com>
2020-08-20 14:54:51 -0700

    upload basic structure

--- tools/dkcm/clerk/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE clusters (
-  clusterid char(6) NOT NULL,
-  accesstoken   char(9) NOT NULL,
-  boskosid  varchar(30) NOT NULL,
-  prowid    varchar(30) NOT NULL,
-  status    varchar(14) NOT NULL,
-  zone    varchar(30) NOT NULL
-);
