10a926260ee6129ead85decbdacc128d25553a62
yizong <yijianzong@gmail.com>
2020-08-19 21:30:25 -0700

    upload basic structure

--- tools/dkcm/clerk/clerk.go
+++ tools/dkcm/clerk/clerk.go
@@ -1,9 +1,9 @@
-package main
+package mainservice
 
 import (
 	"database/sql"
-	"fmt"
-	"log"
+	"math/rand"
+	"time"
 	// _ "github.com/lib/pq"
 )
 
@@ -13,34 +13,67 @@ type Cluster struct {
 	boskosid    string
 	prowid      string
 	status      string
+	zone        string
 }
 
-func main() {
-	db, err := sql.Open("postgres", "user=postgres dbname=postgres sslmode=disable password=newPassword")
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var (
+	db        *sql.DB
+	randomGen *rand.Rand = rand.New(
+		rand.NewSource(time.Now().UnixNano()))
+)
+
+/*
+func init() {
+	var err error
+	db, err = sql.Open("postgres", "user=postgres dbname=postgres sslmode=disable password=newPassword")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	rows, err := db.Query("SELECT * FROM clusters")
-	if err != nil {
+	// check connection
+	if err = db.Ping(); err != nil {
 		log.Fatal(err)
 	}
-	defer rows.Close()
 
-	cls := make([]*Cluster, 0)
-	for rows.Next() {
-		cl := new(Cluster)
-		err := rows.Scan(&cl.clusterid, &cl.accesstoken, &cl.boskosid, &cl.prowid, &cl.status)
-		if err != nil {
-			log.Fatal(err)
+} */
+
+func generateID(idSize int) string {
+	bytes := make([]byte, idSize)
+	for i := range bytes {
+		bytes[i] = charset[randomGen.Intn(len(charset))]
+	}
+	return string(bytes)
+}
+
+func generateUnique(idSize int, key string) string {
+	var randomid string
+	var count int
+	for {
+		randomid = generateID(idSize)
+		db.QueryRow("SELECT count(*) FROM table Where $1 = $2", key, randomid).Scan(&count)
+		if count == 0 {
+			return randomid
 		}
-		cls = append(cls, cl)
 	}
-	if err = rows.Err(); err != nil {
-		log.Fatal(err)
-	}
+}
 
-	for _, cl := range cls {
-		fmt.Printf("%s, %s, %s, %s, %s\n", cl.clusterid, cl.accesstoken, cl.boskosid, cl.prowid, cl.status)
-	}
+func clerkInfo() {
+	// print the database
+}
+
+func clerkQuery() (bool, string, string) {
+	// check whether available cluster exists
+	return true, "", ""
+}
+
+func getWithToken(token string, status chan string, errc chan string) {
+	// check in with token
+
+}
+
+func updateCluster(zone string, prowid string, boskosid string, infoChan chan string) {
+	// assign cluster if available
+
 }
--- tools/dkcm/mainservice/main_service.go
+++ tools/dkcm/mainservice/main_service.go
@@ -1,12 +1,8 @@
 package mainservice
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"net/url"
-	"strconv"
 	"time"
 )
 
@@ -28,76 +24,15 @@ func releaseProject() bool {
 }
 
 func pollProject() string {
-	data := url.Values{}
-	resp, err := http.PostForm("http://localhost:8080/acquire?type=prow-cluster&state=free&dest=busy&owner=prow", data)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	var res map[string]interface{}
-	json.NewDecoder(resp.Body).Decode(&res)
-	fmt.Println(res)
-	return fmt.Sprint(res["name"])
+	return "Boskos Project"
 }
 
 func updateClerk(config requestInfo, prowid string) string {
-	tokenChannel := make(chan string)
-	boskosid := pollProject()
-	go updateCluster(config.zone, prowid, boskosid, tokenChannel)
-	return <-tokenChannel
+	return "response token"
 }
 
 func handleProw(w http.ResponseWriter, req *http.Request) {
-	switch req.Method {
-	case "GET":
-		w.Write([]byte("we are checking whether your cluster has been created\n"))
-		var token string
-		for k, v := range req.URL.Query() {
-			fmt.Printf("%s: %s\n", k, v)
-			if k == "token" {
-				token = v[0]
-			}
-		}
-		statusChannel := make(chan string, 5)
-		errorChannel := make(chan string, 1)
-		go getWithToken(token, statusChannel, errorChannel)
-	ForLoop:
-		for {
-			select {
-			case err := <-errorChannel:
-				w.Write([]byte(err))
-				break ForLoop
-			case status := <-statusChannel:
-				w.Write([]byte(status))
-				for val := range statusChannel {
-					w.Write([]byte(val))
-				}
-				break ForLoop
-			default:
-				w.Write([]byte("waiting for clerk to verify...\n"))
-				time.Sleep(1 * time.Millisecond)
-			}
-		}
-	case "POST":
-		prowid := req.PostFormValue("prowid")
-		minNodes, err1 := strconv.Atoi(req.PostFormValue("minNodes"))
-		maxNodes, err2 := strconv.Atoi(req.PostFormValue("maxNodes"))
-		if err1 != nil {
-			panic(err1)
-		}
-		if err2 != nil {
-			panic(err2)
-		}
-		nodeType := req.PostFormValue("nodeType")
-		zone := req.PostFormValue("zone")
-		configInfo := requestInfo{minNodes, maxNodes, nodeType, zone}
-		received := "configuration: nodeType: " + nodeType + " prow id:" + prowid + " zone: " + zone + "\n"
-		//fmt.Println(received)
-		w.Write([]byte(received))
-		accessToken := updateClerk(configInfo, prowid)
-		w.Write([]byte("your accesssToken to the cluster is : " + accessToken + "\n"))
-	}
-	clerkInfo()
+	// handle prow requests
 
 }
 
