5a8105609141d05977e245722028e44eacdbc305
Yijian Zong <yijianzong@gmail.com>
2020-09-08 11:29:32 -0700

    functions implementation (#2404)

--- tools/dkcm/clerk/clerk.go
+++ tools/dkcm/clerk/clerk.go
@@ -18,35 +18,39 @@ package clerk
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 	"time"
 
 	"k8s.io/apimachinery/pkg/util/uuid"
 	"knative.dev/test-infra/pkg/mysql"
 )
 
+// ClusterParams is a struct for common cluster parameters shared by both Cluster and Request
+type ClusterParams struct {
+	Zone     string
+	Nodes    int64
+	NodeType string
+}
+
 // Cluster stores a row in the "Cluster" db table
 type Cluster struct {
-	ClusterID string
+	*ClusterParams
+	ClusterID int64
 	ProjectID string
 	Status    string
-	Zone      string
-	Nodes     int
-	NodeType  string
 }
 
 // Request stores a request for the "Request" table
 type Request struct {
+	*ClusterParams
 	accessToken string
 	requestTime time.Time
-	Zone        string
-	Nodes       int
-	NodeType    string
 	ProwJobID   string
-	ClusterID   string
+	ClusterID   int64
 }
 
-// ClusterConfig is the struct to return to Prow once the cluster is available
-type ClusterConfig struct {
+// Response is the struct to return(end product) to Prow once the cluster is available
+type Response struct {
 	ClusterName string
 	ProjectID   string
 	Zone        string
@@ -54,25 +58,30 @@ type ClusterConfig struct {
 
 type ClerkOperations interface {
 	//check cluster available, if available, return cluster id and access token
-	CheckAvail(nodes int, nodeType string, zone string) (bool, string, string)
-	// get with token
-	GetCluster(accessToken string) (ClusterConfig, error)
+	CheckAvail(cp ClusterParams) (bool, int64)
+	// get with cluster id stored in the Cluster database
+	GetCluster(clusterID int64) (Response, error)
 	// delete a cluster entry
-	DeleteCluster(accessToken string) error
+	DeleteCluster(clusterID int64) error
 	// Insert a cluster entry
-	InsertCluster(nodes int, nodeType string, projectID string, status string, zone string) error
+	InsertCluster(c *Cluster) error
 	// Update a cluster entry
-	UpdateCluster(clusterID string, fields []string, values []string) error
-	// List clutsers within a time interval (use for checking after downtime to see stale clusters)
-	ListClusters(window time.Duration) ([]Cluster, error)
+	UpdateCluster(clusterID int64, opts ...UpdateOption) error
+	// List clutsers (use for checking after downtime to see stale clusters)
+	ListClusters() ([]Cluster, error)
 	// Generate Unique access token for Prow
 	generateToken() string
 	// Insert a request entry
-	InsertRequest(Zone string, Nodes int, NodeType string, ProwJobID string) error
+	InsertRequest(r *Request) error
 	// Update a request entry
-	UpdateRequest(requestID string, fields []string, values []string) error
+	UpdateRequest(requestID int64, opts ...UpdateOption) error
 	// List requests within a time interval (use for checking after downtime to see stale requests)
 	ListRequests(window time.Duration) ([]Request, error)
+}
+
+// compatible with both Row and Rows and unit test friendly
+type scannable interface {
+	Scan(dest ...interface{}) error
 }
 
 // DB holds an active database connection. This implements all the fuunctions of ClerkOperations.
@@ -81,71 +90,203 @@ type DB struct {
 }
 
 func (c Cluster) String() string {
-	return ""
+	return fmt.Sprintf("Cluster Info: (ProjectID: %s, NodesCount: %d, NodeType: %s, Status: %s, Zone: %s)",
+		c.ProjectID, c.Nodes, c.NodeType, c.Status, c.Zone)
+}
+
+func (r Request) String() string {
+	return fmt.Sprintf("Request Info: (RequestTime: %v, NodesCount: %v, NodeType: %s, ProwJobID: %s, ClusterID: %d,Zone: %s)",
+		r.requestTime, r.Nodes, r.NodeType, r.ProwJobID, r.ClusterID, r.Zone)
+}
+
+// generate a list of strings that could be used in a query for ClusterParams
+func (cp *ClusterParams) generateParamsConditions(opts ...QueryClusterParamsOption) []string {
+	var fieldStatements []string
+	for _, opt := range opts {
+		fieldStatements = append(fieldStatements, opt(cp))
+	}
+	return fieldStatements
+}
+
+func generateAND(fieldStatements []string) string {
+	return strings.Join(fieldStatements, " AND ")
+}
+
+// Populate fields of Cluster
+func populateCluster(sc scannable) (*Cluster, error) {
+	c := &Cluster{}
+	err := sc.Scan(&c.ClusterID, &c.ProjectID, &c.Nodes, &c.NodeType, &c.Status, &c.Zone)
+	return c, err
+}
+
+// Populate fields of Request
+func populateRequest(sc scannable) (*Request, error) {
+	r := &Request{}
+	err := sc.Scan(&r.accessToken, &r.requestTime, &r.Nodes, &r.NodeType, &r.ProwJobID, &r.ClusterID, &r.Zone)
+	return r, err
 }
 
 // NewDB returns the DB object with an active database connection
 func NewDB(c *mysql.DBConfig) (*DB, error) {
-	return nil, nil
+	db, err := c.Connect()
+	return &DB{db}, err
 }
 
-func (db *DB) CheckAvail() (bool, string, string) {
-	return true, "", ""
+// check available clusters in Cluster db and return zone and projectID that would be used by ProwJob
+func (db *DB) CheckAvail(cp *ClusterParams) (bool, int64) {
+	// getting query string ready
+	conditions := generateAND(cp.generateParamsConditions(QueryZone(), QueryNodes(), QueryNodeType()))
+	queryString := fmt.Sprintf("SELECT * FROM Cluster WHERE Status = 'Ready' AND %v", conditions)
+	// check whether available cluster exists
+	row := db.QueryRow(queryString)
+	c, err := populateCluster(row)
+	// no available cluster is found
+	if err != nil {
+		return false, -1
+	}
+	return true, c.ClusterID
+
 }
 
 // insert a cluster entry into db
-func (db *DB) InsertCluster(nodes int, nodeType string, accessToken string, projectID string, prowJobID string, status string, zone string) error {
-	return nil
+func (db *DB) InsertCluster(c *Cluster) error {
+	stmt, err := db.Prepare(`INSERT INTO Cluster(Nodes, NodeType, Zone, ProjectID, Status)
+							VALUES (?,?,?,?,?)`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(c.Nodes, c.NodeType, c.Zone, c.ProjectID, c.Status)
+	return err
+}
+
+func updateQueryString(dbName string, id int64, opts ...UpdateOption) string {
+	var fieldStatements []string
+	for _, opt := range opts {
+		fieldStatements = append(fieldStatements, opt())
+	}
+	return fmt.Sprintf("UPDATE %s SET %s WHERE ID = %d", dbName, strings.Join(fieldStatements, ","), id)
 }
 
 // Update a cluster entry on different fields len(fields) == len(values)
-func (db *DB) UpdateCluster(clusterID string, fields []string, values []string) error {
-	return nil
+func (db *DB) UpdateCluster(clusterID int64, opts ...UpdateOption) error {
+	queryString := updateQueryString("Cluster", clusterID, opts...)
+	_, err := db.Exec(queryString)
+	return err
 }
 
-// List all clusters within a certain time window
-func (db *DB) ListClusters(window time.Duration) ([]Cluster, error) {
+// List all clusters
+func (db *DB) ListClusters() ([]Cluster, error) {
 	var result []Cluster
+	rows, err := db.Query(`
+	SELECT ID, Nodes, NodeType, ProjectID, Status, Zone
+	FROM Cluster`)
+	if err != nil {
+		return result, err
+	}
+	for rows.Next() {
+		c, err := populateCluster(rows)
+		if err != nil {
+			return result, err
+		}
+		result = append(result, *c)
+	}
 	return result, nil
 
 }
 
 // DeleteCluster deletes a row from Cluster db
-func (db *DB) DeleteCluster(accessToken string) error {
-	return nil
+func (db *DB) DeleteCluster(clusterID int64) error {
+	// make sure the cluster to deleted exist
+	_, err := db.GetCluster(clusterID)
+	if err != nil {
+		return err
+	}
+	delString := "DELETE FROM Cluster WHERE ID = ?"
+	stmt, err := db.Prepare(delString)
+	if err == nil {
+		// check exactly one row is deleted
+		err = checkAffected(stmt, 1, clusterID)
+		defer stmt.Close()
+	}
+	return err
 }
 
 func (db *DB) generateToken() string {
 	return string(uuid.NewUUID())
 }
 
-func (db *DB) getCluster(token string) (*ClusterConfig, error) {
+// assumption: get cluster only when it is ready, the clusterid that shows up on request db
+func (db *DB) GetCluster(clusterID int64) (*Response, error) {
 	// check in with token
-	return &ClusterConfig{}, nil
+	queryString := "SELECT * FROM Cluster WHERE ID = ?"
+	row := db.QueryRow(queryString, clusterID)
+	c, err := populateCluster(row)
+	if err != nil {
+		return &Response{}, err
+	}
+	clusterName := nameCluster(clusterID)
+	cc := &Response{ClusterName: clusterName, ProjectID: c.ProjectID, Zone: c.Zone}
+	return cc, err
+
 }
 
-// checkRowAffected number of checks row affected by a given query.
-func checkRowAffected(stmt *sql.Stmt, args ...interface{}) error {
+// checkRowAffected expects a certain number of rows in the db to be affected.
+func checkAffected(stmt *sql.Stmt, numRows int64, args ...interface{}) error {
+	res, err := stmt.Exec(args...)
+	if err != nil {
+		return fmt.Errorf("Statement not executable: %v ", err)
+	}
+	if rowsAffected, err := res.RowsAffected(); err != nil {
+		return fmt.Errorf("Fail to get rows affected: %v ", err)
+	} else if rowsAffected != numRows {
+		return fmt.Errorf("Expected %d row affected, got %d", numRows, rowsAffected)
+	}
 	return nil
 }
 
 // name cluster in the following format: e2e-cluster{id}-{prowJobID}
-func nameCluster(clusterID string, prowJobID string) string {
-	return fmt.Sprintf("e2e-cluster%s-%s", clusterID, prowJobID)
+func nameCluster(clusterID int64) string {
+	return fmt.Sprintf("e2e-cluster%v", clusterID)
 }
 
 // insert a request entry
-func (db *DB) InsertRequest(Zone string, Nodes int, NodeType string, ProwJobID string) error {
-	return nil
+func (db *DB) InsertRequest(r *Request) error {
+	stmt, err := db.Prepare(`INSERT INTO Requests(AccessToken, RequestTime, ProwJobID, Nodes, NodeType, Zone)
+							VALUES (?,?,?,?,?,?)`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	accessToken := db.generateToken()
+	_, err = stmt.Exec(accessToken, r.requestTime, r.ProwJobID, r.Nodes, r.NodeType, r.Zone)
+	return err
 }
 
 // Update a request entry  len(fields) == len(values)
-func (db *DB) UpdateRequest(requestID string, fields []string, values []string) error {
-	return nil
+func (db *DB) UpdateRequest(requestID int64, opts ...UpdateOption) error {
+	queryString := updateQueryString("Request", requestID, opts...)
+	_, err := db.Exec(queryString)
+	return err
 }
 
 // List requests within a time interval (use for checking after downtime to see stale requests)
 func (db *DB) ListRequests(window time.Duration) ([]Request, error) {
 	var result []Request
+	startTime := time.Now().Add(-1 * window)
+	rows, err := db.Query(`
+	SELECT ID, RequestTime, Nodes, NodeType, ProwJobID, Zone, ClusterID
+	FROM Request
+	WHERE RequestTime > ?`, startTime)
+	if err != nil {
+		return result, err
+	}
+	for rows.Next() {
+		r, err := populateRequest(rows)
+		if err != nil {
+			return result, err
+		}
+		result = append(result, *r)
+	}
 	return result, nil
 }
---
+++ tools/dkcm/clerk/schema.sql
@@ -0,0 +1,43 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+CREATE TABLE Clusters (
+  ID int NOT NULL AUTO_INCREMENT,
+  ProjectID varchar(1023) NOT NULL,
+  Status varchar(1023) DEFAULT 'WIP',
+  Zone varchar(1023) NOT NULL,
+  Nodes int NOT NULL,
+  NodeType varchar(1023) NOT NULL,
+  PRIMARY KEY (ID)
+
+);
+
+CREATE TABLE Requests (
+  ID int NOT NULL AUTO_INCREMENT,
+  AccessToken varchar(1023) NOT NULL,
+  RequestTime timestamp,
+  Zone varchar(1023) NOT NULL,
+  Nodes varchar(1023) NOT NULL,
+  NodeType varchar(1023) NOT NULL,
+  ProwJobID varchar(1023) NOT NULL,
+  ClusterID int,
+  CONSTRAINT FkCluster 
+  FOREIGN KEY (ClusterID) 
+      REFERENCES Clusters(ID)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+  PRIMARY KEY (ID)
+
+);
