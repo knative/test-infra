e7767ca30d28fc14c16e26d587f3d5597a38442e
Joshua Bone <joshuabone@google.com>
2020-11-11 13:18:33 -0800

    script uses correct paths

--- config/prod/prow/k8s-testgrid/generate-k8s-testgrid.sh
+++ config/prod/prow/k8s-testgrid/generate-k8s-testgrid.sh
@@ -14,18 +14,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CONFIG_YAML="$(mktemp)" # randomize the name of the local config file we will write
+source $(dirname $0)/../../../../scripts/library.sh
+
+set -e
+
+make -C "${REPO_ROOT_DIR}/config/prod" get-cluster-credentials
+trap "make -C '${REPO_ROOT_DIR}/config/prod' unset-cluster-credentials" EXIT
+
+PROW_CONFIG="$(mktemp)"
+PROW_JOB_CONFIG="${REPO_ROOT_DIR}/config/prod/prow/jobs/config.yaml"
+TESTGRID_YAML="${REPO_ROOT_DIR}/config/prod/prow/k8s-testgrid/k8s-testgrid.yaml"
 
 kubectl get configmaps config -o "jsonpath={.data['config\.yaml']}" >"${CONFIG_YAML}"
 echo "Prow core config downloaded at ${CONFIG_YAML}"
 
 docker run -i --rm \
     -v "${PWD}:${PWD}" \
-    -v "${CONFIG_YAML}:${CONFIG_YAML}" \
+    -v "${PROW_CONFIG}:${PROW_CONFIG}" \
+    -v "${PROW_JOB_CONFIG}:${PROW_JOB_CONFIG}" \
+    -v "${TESTGRID_YAML}:${TESTGRID_YAML}" \
     -w "${PWD}" \
     gcr.io/k8s-prow/transfigure:v20201110-9e512b5af0 \
     "/etc/github-token/oauth" \
-    "${CONFIG_YAML}" \
-    "config/prod/prow/jobs/config.yaml" \
-    "config/prod/prow/k8s-testgrid/k8s-testgrid.yaml" \
+    "${PROW_CONFIG}" \
+    "${PROW_JOB_CONFIG}" \
+    "${TESTGRID_YAML}" \
     "knative"
