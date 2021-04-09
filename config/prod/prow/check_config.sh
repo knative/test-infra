f6108fb7ce82a3315632f3e72fb712234ebec93c
Knative Prow Updater Robot <knative-prow-updater-robot@google.com>
2021-04-09 17:16:11 +0000

    Update Prow to v20210409-985ef5e721

--- config/prod/prow/check_config.sh
+++ config/prod/prow/check_config.sh
@@ -57,7 +57,7 @@ REPO_YAML_PATH_ARG=""
 docker run -i --rm \
     -v "${PWD}:${PWD}" -v "${CONFIG_YAML}:${CONFIG_YAML}" -v "${PLUGINS_YAML}:${PLUGINS_YAML}" -v "${JOB_YAML}:${JOB_YAML}" \
     -w "${PWD}" \
-    gcr.io/k8s-prow/checkconfig:v20210323-4a95c1446c \
+    gcr.io/k8s-prow/checkconfig:v20210409-985ef5e721 \
     "--config-path=${CONFIG_YAML}" "--job-config-path=${JOB_YAML}" \
     "--plugin-config=${PLUGINS_YAML}" "--strict" "--exclude-warning=mismatched-tide" \
     "--exclude-warning=long-job-names" \
