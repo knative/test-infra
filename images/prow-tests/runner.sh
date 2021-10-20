ab68d8b380ea7aea4ef728de646bd0b1f66af5f0
Chi Zhang <chizhg@google.com>
2021-10-20 03:35:03 +0000

    Fix go coverage tool by using the correct GOPATH

--- images/prow-tests/runner.sh
+++ images/prow-tests/runner.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ORIGINAL_GOPATH="${HOME}/go"
+ORIGINAL_GOPATH="/home/prow/go"
 
 source "${HOME}/.gvm/scripts/gvm"
 
