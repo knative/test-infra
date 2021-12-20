1b6585c46d568fc53d085011b63a8ee3d4bd2e1a
Knative Prow Updater Robot <knative-prow-updater-robot@google.com>
2021-12-20 00:30:32 +0000

    Bumping Prow
    Multiple distinct gcr.io/k8s-prow/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/d726a04d2c...468aed6c0c | 2021&#x2011;12&#x2011;13&nbsp;&#x2192;&nbsp;2021&#x2011;12&#x2011;17 | branchprotector, checkconfig, config-bootstrapper, configurator, generic-autobumper, hook, label_sync, mkpj, peribolos

--- config/prod/prow/run_job.sh
+++ config/prod/prow/run_job.sh
@@ -51,7 +51,7 @@ if [[ -n "${GITHUB_TOKEN_PATH}" ]]; then
         -v "${PWD}:${PWD}" -v "${CONFIG_YAML}:${CONFIG_YAML}" -v "${JOB_CONFIG_YAML}:${JOB_CONFIG_YAML}" \
         -v "${GITHUB_TOKEN_PATH}:${GITHUB_TOKEN_PATH}" \
         -w "${PWD}" \
-        gcr.io/k8s-prow/mkpj:v20211213-d726a04d2c \
+        gcr.io/k8s-prow/mkpj:v20211217-468aed6c0c \
         "--job=${JOB_NAME}" "--config-path=${CONFIG_YAML}" "--job-config-path=${JOB_CONFIG_YAML}" \
         "--github-token-path=${GITHUB_TOKEN_PATH}" \
         > ${JOB_YAML}
@@ -60,7 +60,7 @@ else
     docker run -i --rm \
         -v "${PWD}:${PWD}" -v "${CONFIG_YAML}:${CONFIG_YAML}" -v "${JOB_CONFIG_YAML}:${JOB_CONFIG_YAML}" \
         -w "${PWD}" \
-        gcr.io/k8s-prow/mkpj:v20211213-d726a04d2c \
+        gcr.io/k8s-prow/mkpj:v20211217-468aed6c0c \
         "--job=${JOB_NAME}" "--config-path=${CONFIG_YAML}" "--job-config-path=${JOB_CONFIG_YAML}" \
         > ${JOB_YAML} || failed=1
 
