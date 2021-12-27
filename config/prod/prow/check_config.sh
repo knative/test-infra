d7b55fd9930b254338131dc603aae9ed1c1c51a2
Knative Prow Updater Robot <knative-prow-updater-robot@google.com>
2021-12-27 19:15:27 +0000

    Bumping Prow
    Multiple distinct gcr.io/k8s-prow/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/0fa9317992...3b9bf17c27 | 2021&#x2011;12&#x2011;24&nbsp;&#x2192;&nbsp;2021&#x2011;12&#x2011;24 | branchprotector, checkconfig, config-bootstrapper, configurator, generic-autobumper, hook, label_sync, mkpj, peribolos

--- config/prod/prow/check_config.sh
+++ config/prod/prow/check_config.sh
@@ -57,7 +57,7 @@ REPO_YAML_PATH_ARG=""
 docker run -i --rm \
     -v "${PWD}:${PWD}" -v "${CONFIG_YAML}:${CONFIG_YAML}" -v "${PLUGINS_YAML}:${PLUGINS_YAML}" -v "${JOB_YAML}:${JOB_YAML}" \
     -w "${PWD}" \
-    gcr.io/k8s-prow/checkconfig:v20211224-0fa9317992 \
+    gcr.io/k8s-prow/checkconfig:v20211224-3b9bf17c27 \
     "--config-path=${CONFIG_YAML}" "--job-config-path=${JOB_YAML}" \
     "--plugin-config=${PLUGINS_YAML}" "--strict" "--exclude-warning=mismatched-tide" \
     "--exclude-warning=long-job-names" \
