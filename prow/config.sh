9f9eaad03bc15c0739797ac657b33d2df36dcc96
Knative Prow Updater Robot <knative-prow-updater-robot@google.com>
2022-03-07 07:18:35 -0800

    Bumping Prow (#3136)
    Multiple distinct gcr.io/k8s-prow/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/e131bfaf16...21d4df3f17 | 2022&#x2011;02&#x2011;25&nbsp;&#x2192;&nbsp;2022&#x2011;03&#x2011;03 | branchprotector, checkconfig, cherrypicker, clonerefs, configurator, crier, deck, entrypoint, generic-autobumper, ghproxy, hook, horologium, initupload, label_sync, mkpj, needs-rebase, peribolos, prow-controller-manager, sidecar, sinker, status-reconciler, tide

--- prow/config.sh
+++ prow/config.sh
@@ -69,7 +69,7 @@ else #check
 	docker run -i --rm \
 	    -v "${PWD}:${PWD}" -v "${CONFIG_YAML}:${CONFIG_YAML}" -v "${PLUGINS_YAML}:${PLUGINS_YAML}" -v "${JOB_YAML}:${JOB_YAML}" \
 	    -w "${PWD}" \
-	    gcr.io/k8s-prow/checkconfig:v20220225-e131bfaf16 \
+	    gcr.io/k8s-prow/checkconfig:v20220303-21d4df3f17 \
 	    "--config-path=${CONFIG_YAML}" "--job-config-path=${JOB_YAML}" \
 	    "--plugin-config=${PLUGINS_YAML}" "--strict" "--exclude-warning=mismatched-tide" \
 	    "--exclude-warning=long-job-names" \
