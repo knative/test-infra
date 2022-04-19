e51e07fbeec546a559b1ecedcc4359ec87edde04
upodroid <cy@borg.dev>
2022-04-19 18:01:41 +0100

    write the other clusters

--- infra/gcp/tests/gke.tf
+++ infra/gcp/tests/gke.tf
@@ -35,16 +35,99 @@ module "prow_trusted" {
       initial_node_count = 1
     },
   ]
+}
 
-  node_pools_oauth_scopes = {
-    prow-trusted = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
+module "prow" {
+  source                     = "terraform-google-modules/kubernetes-engine/google"
+  project_id                 = module.project.project_id
+  name                       = "prow"
+  region                      = "us-central1"
+  release_channel            = "RAPID"
+  network                    = "default"
+  subnetwork                 = "default"
+  ip_range_pods              = ""
+  ip_range_services          = ""
+  http_load_balancing        = true
+  network_policy             = false
+  horizontal_pod_autoscaling = true
+  filestore_csi_driver       = false
+  create_service_account     = false
+
+  node_pools = [
+    {
+      name               = "prod-v1"
+      machine_type       = "e2-standard-8"
+      min_count          = 1
+      max_count          = 3
+      disk_size_gb       = 100
+      disk_type          = "pd-standard"
+      image_type         = "COS_CONTAINERD"
+      auto_repair        = true
+      auto_upgrade       = true
+      service_account    = google_service_account.gke_nodes.email
+      enable_secure_boot = true
+      initial_node_count = 1
+    },
+  ]
 
   node_pools_labels = {
-    prow-trusted = {
-      cluster     = "prow-trusted"
+    all = {
+      cluster     = "prow"
+      environment = "production"
+    }
+  }
+}
+
+module "prow_build" {
+  source                     = "terraform-google-modules/kubernetes-engine/google"
+  project_id                 = module.project.project_id
+  name                       = "prow-build"
+  region                     = "us-central1"
+  release_channel            = "RAPID"
+  network                    = "default"
+  subnetwork                 = "default"
+  ip_range_pods              = ""
+  ip_range_services          = ""
+  http_load_balancing        = true
+  network_policy             = false
+  horizontal_pod_autoscaling = true
+  filestore_csi_driver       = false
+  create_service_account     = false
+
+  node_pools = [
+    {
+      name               = "system-pool-v1"
+      machine_type       = "e2-standard-8"
+      min_count          = 1
+      max_count          = 6
+      disk_size_gb       = 100
+      disk_type          = "pd-standard"
+      image_type         = "COS_CONTAINERD"
+      auto_repair        = true
+      auto_upgrade       = true
+      service_account    = google_service_account.gke_nodes.email
+      enable_secure_boot = true
+      initial_node_count = 1
+    },
+    {
+      name               = "testing-pool-v1"
+      machine_type       = "e2-standard-16"
+      min_count          = 0
+      max_count          = 4
+      disk_size_gb       = 100
+      disk_type          = "pd-ssd"
+      image_type         = "COS_CONTAINERD"
+      auto_repair        = true
+      auto_upgrade       = true
+      service_account    = google_service_account.gke_nodes.email
+      enable_secure_boot = true
+      initial_node_count = 0
+    },
+  ]
+
+  node_pools_labels = {
+    all = {
+      cluster     = "prow-build"
       environment = "production"
     }
   }
