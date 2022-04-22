5f1a4b5d537a3b3f240e5af9368edc614423ef43
Mahamed <cy@borg.dev>
2022-04-22 15:10:31 +0100

    Rebuild `prow-build` cluster (#3271)
    * write the other clusters
    
    * fix typos
    
    * add zones to nodepools
    
    * add vpc
    
    * redeploy prow-build

---
+++ infra/gcp/tests/prow.tf
@@ -0,0 +1,36 @@
+// Prow Control Plane
+resource "google_service_account_iam_binding" "prow_control_plane" {
+  service_account_id = google_service_account.prow_control_plane.name
+  role               = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "serviceAccount:knative-tests.svc.id.goog[default/crier]",
+    "serviceAccount:knative-tests.svc.id.goog[default/deck]",
+    "serviceAccount:knative-tests.svc.id.goog[default/sinker]",
+  ]
+}
+
+resource "google_service_account" "prow_control_plane" {
+  account_id   = "prow-control-plane"
+  display_name = "Prow Control Plane"
+  description = "Service account used by Prow control plane to interact with Google services for the knative-tests project."
+  project      = "knative-tests"
+}
+
+
+// External Secrets Operator
+resource "google_service_account_iam_binding" "external_secrets" {
+  service_account_id = google_service_account.external_secrets.name
+  role               = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "serviceAccount:knative-tests.svc.id.goog[default/kubernetes-external-secrets-sa]",
+    "serviceAccount:knative-tests.svc.id.goog[default/external-secrets]",
+  ]
+}
+
+resource "google_service_account" "external_secrets" {
+  account_id   = "kubernetes-external-secrets-sa"
+  display_name = "External Secrets Operator"
+  project      = "knative-tests"
+}
