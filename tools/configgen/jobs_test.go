9ee2f300043b3ccc07e0d79c7fb4c53d7401f2d9
upodroid <cy@borg.dev>
2022-04-25 19:30:14 +0100

    allow test to pass & fix typos

--- tools/configgen/jobs_test.go
+++ tools/configgen/jobs_test.go
@@ -106,7 +106,7 @@ func TestReleaseJobs(t *testing.T) {
 					errStrs.WriteString(fmt.Sprintf("cron is supposed to be auto-generated, do not add it for release job in %q\n", path))
 				}
 				reqs := sets.NewString(job.Requirements...)
-				if !reqs.Has("release") {
+				if !reqs.Has("release") || !reqs.Has("release-dev") {
 					errStrs.WriteString(fmt.Sprintf("release requirement is required for release job in %q\n", path))
 				}
 
