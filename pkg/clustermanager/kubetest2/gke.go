d4a62c2039d3bc6ab5cd5507293b00cdc43b94ae
Paul Schweigert <paulschw@us.ibm.com>
2022-04-28 13:58:53 -0400

    updates deprecated cluster --version flag (#3298)
    Signed-off-by: Paul S. Schweigert <paulschw@us.ibm.com>
    
    GKE has deprecated the --version flag in favor of --cluster-version.
    
    Link to GKE docs for flag to use:
    https://cloud.google.com/kubernetes-engine/versioning#specifying_cluster_version
    Example of deprecation warning in Prow job:
    https://prow.knative.dev/view/gs/knative-prow/pr-logs/pull/knative_serving/12889/istio-latest-no-mesh_serving_main/1519674948770074624#1:build-log.txt%3A330
    for an example.

--- pkg/clustermanager/kubetest2/gke.go
+++ pkg/clustermanager/kubetest2/gke.go
@@ -95,7 +95,7 @@ func Run(opts *Options, cc *GKEClusterConfig) error {
 	if cc.ReleaseChannel != "" {
 		createCommand += " --release-channel=" + cc.ReleaseChannel
 	}
-	kubetest2Flags = append(kubetest2Flags, "--version="+cc.Version)
+	kubetest2Flags = append(kubetest2Flags, "--cluster-version="+cc.Version)
 	if cc.Addons != "" {
 		createCommand += " --addons=" + cc.Addons
 	}
