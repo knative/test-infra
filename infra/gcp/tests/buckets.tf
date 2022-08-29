caf8ce5bd698b1ddc0166e27ba053e58fb130f64
upodroid <cy@borg.dev>
2022-08-29 23:02:32 +0100

    add go workspaces, switch to workload identity

--- infra/gcp/tests/buckets.tf
+++ infra/gcp/tests/buckets.tf
@@ -44,3 +44,49 @@ module "iam_prow_bucket" {
     ]
   }
 }
+
+resource "google_storage_bucket" "testgrid" {
+  name                        = "knative-own-testgrid"
+  location                    = "US"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      age = 210
+    }
+  }
+
+  lifecycle_rule {
+    action {
+      storage_class = "NEARLINE"
+      type          = "SetStorageClass"
+    }
+    condition {
+      age = 180
+    }
+  }
+
+}
+
+module "iam_testgrid_bucket" {
+  source          = "terraform-google-modules/iam/google//modules/storage_buckets_iam"
+  storage_buckets = [google_storage_bucket.testgrid.name]
+  version         = "~> 7"
+
+  mode = "authoritative"
+
+  bindings = {
+    "roles/storage.admin" = [
+      "serviceAccount:testgrid-updater@knative-tests.iam.gserviceaccount.com",
+      "serviceAccount:updater@k8s-testgrid.iam.gserviceaccount.com"
+    ]
+    "roles/storage.objectViewer" = [
+      "serviceAccount:k8s-testgrid@appspot.gserviceaccount.com"
+    ]
+  }
+}
