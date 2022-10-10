b1a6edc702c75c16c03aa030e0bd29af553b698e
Knative Prow Updater Robot <knative-prow-updater-robot@google.com>
2022-10-10 15:15:38 +0000

    Bumping Prow and Boskos
    Multiple distinct gcr.io/k8s-prow/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/4982ca61ab...08c3d7a2a6 | 2022&#x2011;09&#x2011;30&nbsp;&#x2192;&nbsp;2022&#x2011;10&#x2011;07 | branchprotector, checkconfig, cherrypicker, clonerefs, config-bootstrapper, configurator, crier, deck, entrypoint, generic-autobumper, ghproxy, hook, horologium, initupload, label_sync, mkpj, needs-rebase, peribolos, prow-controller-manager, sidecar, sinker, status-reconciler, tide
    
    Multiple distinct gcr.io/k8s-staging-boskos/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes-sigs/boskos/compare/1b2c80c...2b58a7a | 2022&#x2011;09&#x2011;23&nbsp;&#x2192;&nbsp;2022&#x2011;10&#x2011;04 | boskos, janitor, reaper

--- prow/jobs/run_job.sh
+++ prow/jobs/run_job.sh
@@ -39,7 +39,7 @@ if [[ -n "${GITHUB_TOKEN_PATH}" ]]; then
         -v "${PWD}:${PWD}" -v "${CONFIG_YAML}:${CONFIG_YAML}" -v "${JOB_CONFIG_YAML}:${JOB_CONFIG_YAML}" \
         -v "${GITHUB_TOKEN_PATH}:${GITHUB_TOKEN_PATH}" \
         -w "${PWD}" \
-        gcr.io/k8s-prow/mkpj:v20220930-4982ca61ab \
+        gcr.io/k8s-prow/mkpj:v20221007-08c3d7a2a6 \
         "--job=${JOB_NAME}" "--config-path=${CONFIG_YAML}" "--job-config-path=${JOB_CONFIG_YAML}" \
         "--github-token-path=${GITHUB_TOKEN_PATH}" \
         > "${JOB_YAML}"
@@ -48,7 +48,7 @@ else
     docker run -i --rm \
         -v "${PWD}:${PWD}" -v "${CONFIG_YAML}:${CONFIG_YAML}" -v "${JOB_CONFIG_YAML}:${JOB_CONFIG_YAML}" \
         -w "${PWD}" \
-        gcr.io/k8s-prow/mkpj:v20220930-4982ca61ab \
+        gcr.io/k8s-prow/mkpj:v20221007-08c3d7a2a6 \
         "--job=${JOB_NAME}" "--config-path=${CONFIG_YAML}" "--job-config-path=${JOB_CONFIG_YAML}" \
         > "${JOB_YAML}" || failed=1
 
