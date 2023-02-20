8e42b241e88b9d4e49c1af11d1fa57f2d3c5f6bc
Evan Anderson <evana@vmware.com>
2023-02-20 04:35:20 -0800

    Refactor selector with feedback from 3696 (#3704)
    * Refactor selector with feedback from 3696
    
    * It turns out we only used Selector one place, which was inside the module.  Replaced it with a simpler implementation.

--- pkg/gomod/selector.go
+++ pkg/gomod/selector.go
@@ -1,11 +1,10 @@
 package gomod
 
 import (
-	"log"
 	"strings"
-	"sync"
 
 	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"knative.dev/test-infra/pkg/gowork"
 )
 
@@ -15,90 +14,49 @@ var (
 )
 
 // Matcher matches a module name.
-type Matcher interface {
-	// Match returns true if the module name matches.
-	Match(modname string) bool
-}
-
-// MatcherFunc is a function that implements the Matcher interface.
-func MatcherFunc(fn matcherFunc) Matcher {
-	return fn
-}
-
-type matcherFunc func(modname string) bool
-
-func (fn matcherFunc) Match(modname string) bool {
-	return fn(modname)
-}
-
-// Selector is a set of matchers that can be used to select a subset of modules.
-type Selector struct {
-	Includes []Matcher
-	Excludes []Matcher
-}
-
-// Select returns true if the module should be selected.
-func (s Selector) Select(modname string) bool {
-	selected := false
-	for _, m := range s.Includes {
-		if m.Match(modname) {
-			selected = true
-			break
-		}
-	}
-	if !selected {
-		return false
-	}
-	for _, m := range s.Excludes {
-		if m.Match(modname) {
-			return false
-		}
-	}
-	return true
-}
+type Matcher func(modname string) bool
 
 // CurrentModulesMatcher matches the current project modules.
-type CurrentModulesMatcher struct {
-	once sync.Once
-	mods []gowork.Module
-	err  error
-}
+func CurrentModulesMatcher() (Matcher, error) {
+	os := gowork.RealSystem{}
+	knownMods := sets.NewString()
+	mods, err := gowork.List(os, os)
+	if err != nil {
+		if !errors.Is(err, gowork.ErrInvalidGowork) {
+			return nil, err
+		}
+		m, err := gowork.Current(os, os)
+		if err != nil {
+			return nil, err
+		}
+		if m != nil {
+			mods = []gowork.Module{*m}
+		}
+	}
 
-func (c *CurrentModulesMatcher) Match(modname string) bool {
-	c.once.Do(func() {
-		os := gowork.RealSystem{}
-		c.mods, c.err = gowork.List(os, os)
-		if c.err != nil && errors.Is(c.err, gowork.ErrInvalidGowork) {
-			var m *gowork.Module
-			m, c.err = gowork.Current(os, os)
-			if m != nil {
-				c.mods = []gowork.Module{*m}
-			}
-		}
-	})
-	if c.err != nil {
-		log.Fatal(c.err)
+	for _, m := range mods {
+		knownMods.Insert(m.Name)
 	}
-	for _, m := range c.mods {
-		if m.Name == modname {
-			return true
-		}
-	}
-	return false
+
+	return knownMods.Has, nil
 }
 
 // DefaultSelector returns a selector that includes modules with given domain,
 // but excludes the current project modules.
-func DefaultSelector(domain string) (Selector, error) {
+func DefaultSelector(domain string) (Matcher, error) {
 	domain = strings.TrimSpace(domain)
 	if len(domain) == 0 {
-		return Selector{}, ErrNoDomain
+		return func(string) bool { return true }, ErrNoDomain
 	}
 
-	return Selector{
-		Includes: []Matcher{MatcherFunc(func(modname string) bool {
-			return strings.HasPrefix(modname, domain)
-		})},
-		Excludes: []Matcher{&CurrentModulesMatcher{}},
-	}, nil
+	currentModules, err := CurrentModulesMatcher()
+	if err != nil {
+		return nil, err
+	}
+
+	inDomainButNotCurrentWorkspace := func(modname string) bool {
+		return strings.HasPrefix(modname, domain) && !currentModules(modname)
+	}
+
+	return inDomainButNotCurrentWorkspace, nil
 }
